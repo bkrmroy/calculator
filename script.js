4f0049df7d6c901197e6602ecd7a7aafb69c8f5e
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-06-15 22:24:38 +0600

    make the calculator do basic things

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const btnNumber = document.querySelectorAll("[data-number]");
 const display = document.querySelector('.display');
 const btnOperator = document.querySelectorAll("[data-operator]");
-
+const equal = document.querySelector(".equal");
+const smallScreen = document.querySelector(".small-screen");
+const ac = document.querySelector(".ac");
+const del = document.querySelector(".del");
+const decimal = document.querySelector(".decimal");
 
 function add(a,b){
     return a+b;
@@ -16,52 +20,139 @@ function divide(a,b){
     return a/b;
 }
 
+let result;
+
 function operate(a, op, b){
     if(op === '+'){
-        return add(a,b);
+        result =  add(a,b);
     }
     if( op === '-'){
-        return subtract(a,b);
+        result = subtract(a,b);
     }
     if(op === '*'){
-        return multiply(a,b);
+        result =  multiply(a,b);
     }
     if(op === '/'){
-        return divide(a,b);
+        result = divide(a,b);
     }
+    console.log(result)
+
+    if(result === Math.trunc(result)){
+        return result;
+    }
+    else{
+        return result.toFixed(4);
+    }
+    
 }
 
-let num1 = "";
-let operator = '';
-let num2 = '';
 
-function takeNum1(){
-    btnNumber.forEach(item => {
-        item.addEventListener('click',()=>{
-            num1 += item.textContent;
-            display.textContent = num1;
+let clickCount = 1; 
+function takeNum(){
+        btnNumber.forEach(item => {
+            item.addEventListener('click',()=>{
+                if(clickCount === 1){
+                    if(display.textContent === '0.'){
+                        display.textContent = '0.' + item.textContent;
+                    }
+                    else display.textContent = '' + item.textContent;
+                    
+                }
+                else{
+                    display.textContent += item.textContent;
+                }
+                clickCount += 1;
+            })
         })
-    })
+        takeDecimal();
+
 }
+function takeDecimal(){
+    decimal.addEventListener("click",() =>{
+        if(display.textContent === '0'){
+            display.textContent = '0' + decimal.textContent;
+        }
+        else display.textContent += decimal.textContent;
+        
+    },)
+}
+
+
+let numbers = [];
+let operator = [];
+let operatorClickCount = 1;
+
 function takeOperator(){
     btnOperator.forEach(item => {
         item.addEventListener('click',()=>{
-            operator = item.textContent;
-            display.textContent = operator;
+            /*
+            if(display.textContent != ''){
+                numbers.push(display.textContent);
+            }*/
+            
+            //smallScreen.textContent += ''+ numbers[numbers.length - 1]+ '';
+
+            
+            if(operatorClickCount === 1 && display.textContent != ''){
+                operator.push(item.textContent);
+                console.log('operator = '+ operator);
+                numbers.push(display.textContent);
+            }
+            if(operatorClickCount !== 1 && display.textContent != ''){
+                operator.push(item.textContent);
+                console.log('operator = '+ operator);
+
+                numbers.push(display.textContent);
+                let initialSum = operate(Number(numbers[numbers.length - 2]), operator[operator.length - 2], Number(numbers[numbers.length-1]));
+                display.textContent = initialSum;
+                numbers.push(initialSum);
+            }
+            
+            console.log(numbers);
+            console.log(operatorClickCount);
+            operatorClickCount += 1;
+            clickCount= 1;
         })
     })
 }
-/*
-function takeNum2(){
-    for(i=0; i< btnNumber.length; i++){
-        let j = i;
-        btnNumber[i].addEventListener('click',()=>{
-            num2 += btnNumber[j].textContent;
-            display.textContent = num2;
+function printResult(){
+    equal.addEventListener("click",() => {
+        numbers.push(display.textContent);
+        //smallScreen.textContent += display.textContent + '';
 
-        })
+        display.textContent = operate(Number(numbers[numbers.length - 2]), operator[operator.length -1], Number(numbers[numbers.length-1]));
+
+        console.log(numbers);
+        numbers = [];
+        operator = [];
+        clickCount = 1;
+        operatorClickCount = 1;
+
+    })
+    
+}
+function calculate(){
+    takeNum();
+    takeOperator();
+    printResult();
+}
+calculate()
+
+ac.addEventListener("click",()=>{
+    display.textContent = '0';
+    numbers = [];
+    operator = [];
+    clickCount = 1;
+    operatorClickCount = 1;
+    
+});
+del.addEventListener("click",()=>{
+    display.textContent = display.textContent.slice(0, -1);
+    if(display.textContent === ''){
+        display.textContent = '0'
     }
-}*/
-takeNum1()
-takeOperator();
+})
+
+
+
 
